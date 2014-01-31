a6b125cfc2bc70be81373a15febab89487790bb4
Alvaro VM <alvarovm@gmail.com>
2014-01-31 12:30:18 +1100

    header, index, footer updated

--- sidebar.php
+++ sidebar.php
@@ -1,27 +1,27 @@
 <?php
 /**
  * The Sidebar containing the main widget areas.
- *
+ *   <div class="row sidebar">
+
  * @package underfoundation
  */
 ?>
-   <div class="row sidebar">
-	<div id="secondary" class="medium-12 columns widget-area" role="complementary">
+	<div id="secondary" class="widget-area" role="complementary">
 		<?php //do_action( 'before_sidebar' ); ?>
-		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
+		<?php if ( ! dynamic_sidebar( 'sidebar-2' ) ) : ?>
 
-			<aside id="search" class="widget widget_search">
+			<aside id="search" class="medium-4 columns widget widget_search">
 				<?php get_search_form(); ?>
 			</aside>
 
-			<aside id="archives" class="widget">
+			<aside id="archives" class="medium-4 columns widget">
 				<h1 class="widget-title"><?php _e( 'Archives', 'underfoundation' ); ?></h1>
 				<ul>
 					<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
 				</ul>
 			</aside>
 
-			<aside id="meta" class="widget">
+			<aside id="meta" class="medium-4 columns widget">
 				<h1 class="widget-title"><?php _e( 'Meta', 'underfoundation' ); ?></h1>
 				<ul>
 					<?php wp_register(); ?>
